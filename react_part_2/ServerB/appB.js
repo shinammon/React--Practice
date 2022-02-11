850839cc62983085fb1ad3f76a230fc3b665a357
shinamon <jammy83091@gmail.com>
2022-02-11 13:36:51 +0800

    react

---
+++ react_part_2/ServerB/appB.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+var cors = require("cors");
+app.use(cors());
+app.listen(5000);
+
+app.get("/getData",function(req,res){
+    res.send('{"id":3 , "qty":200 }')
+})
